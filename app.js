089705d9df4e81d41065d2c62d770b09f70d92e6
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-11-21 18:59:51 -0500

    added liked quotes feature

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const btn = document.getElementById('roll');
+const list =document.querySelector('.quoteList');
+const likeBtn = document.getElementById('like');
+const clear = document.getElementById('clear');
 const adviceID = document.querySelector('span');
 const quote = document.getElementById('advice');
 
 getQuote();
 
+// * Events
+btn.addEventListener('click', getQuote);
+likeBtn.addEventListener('click', saveQuote);
+clear.addEventListener('click', clearTxt);
+
+//*FUNCTIONS
 async function getQuote(){
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
@@ -11,4 +20,12 @@ async function getQuote(){
     adviceID.innerHTML ='#' + data.slip.id;
     quote.innerHTML = '"' + data.slip.advice + '"';
 }
- btn.addEventListener('click', getQuote);
+function saveQuote(){
+    const node = document.createElement('li');
+    node.innerHTML = quote.innerText;
+    list.appendChild(node);
+    //localStorage.setItem('quote', node);
+}
+function clearTxt(){
+    list.innerHTML= "";
+}
