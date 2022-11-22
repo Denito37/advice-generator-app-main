c625be61c66f50db767ee7b4532ad21230deb8ed
Denito37 <69406178+Denito37@users.noreply.github.com>
2022-11-22 11:17:15 -0500

    add roll number counter feature

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const likeBtn = document.getElementById('like');
 const clear = document.getElementById('clear');
 const adviceID = document.querySelector('span');
 const quote = document.getElementById('advice');
+const rollNum =document.getElementById('number');
+let count = 0;
 
 getQuote();
 
@@ -17,15 +19,19 @@ async function getQuote(){
     const res = await fetch("https://api.adviceslip.com/advice");
     const data = await res.json();
 
+    count += 1;
+    rollNum.innerHTML = count;
+
     adviceID.innerHTML ='#' + data.slip.id;
     quote.innerHTML = '"' + data.slip.advice + '"';
 }
 function saveQuote(){
     const node = document.createElement('li');
-    node.innerHTML = quote.innerText;
+    node.innerHTML = quote.innerText; 
     list.appendChild(node);
-    //localStorage.setItem('quote', node);
 }
 function clearTxt(){
     list.innerHTML= "";
+    count = 0;
+    rollNum.innerHTML = count;
 }
